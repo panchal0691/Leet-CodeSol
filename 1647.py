ceb0a19cf34c02c97929ee21e7a31cc63a6ff4c1
Nishant Panchal <120048543+panchal0691@users.noreply.github.com>
2023-09-12 20:59:42 +0530

    Create 1647.py

---
+++ 1647.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minDeletions(self, s: str) -> int:
+        count = sorted(collections.Counter(s).values())
+
+        s = set()
+        total = 0
+        for x in count:
+            while x in s and x >0:
+                x-=1
+                total +=1
+            s.add(x)
+
+        return total
+        
