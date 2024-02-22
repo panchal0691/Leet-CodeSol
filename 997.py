b140f6f251a65b3c922a7eca4a81bbb1fb2432fe
Nishant Panchal <120048543+panchal0691@users.noreply.github.com>
2024-02-22 22:02:16 +0530

    Create 997.py

---
+++ 997.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+        indegree = [0] * (n + 1)
+        outdegree = [0] * (n + 1)
+        
+        for vec in trust:
+            indegree[vec[1]] += 1
+            outdegree[vec[0]] += 1
+        
+        for i in range(1, n + 1):
+            if indegree[i] == n - 1 and outdegree[i] == 0:
+                return i
+        
+        return -1
+        
