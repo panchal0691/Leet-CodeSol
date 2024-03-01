fef0f2fb1d053bb9dc272f2d23584372a61b7292
Nishant Panchal <120048543+panchal0691@users.noreply.github.com>
2024-03-01 23:46:54 +0530

    Create 2864.py

---
+++ 2864.py
@@ -0,0 +1,6 @@
+class Solution:
+    def maximumOddBinaryNumber(self, s: str) -> str:
+       counts = Counter(s)
+       return '1' * (counts['1'] - 1) + '0' * (counts['0']) + '1'
+       #time O(n)
+       #space O(1)
