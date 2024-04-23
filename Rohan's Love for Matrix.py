4739e1dc2c00098d8ca370bc7763b13a62587768
Nishant Panchal <120048543+panchal0691@users.noreply.github.com>
2024-04-23 23:48:14 +0530

    Create Rohan's Love for Matrix.py

---
+++ Rohan's Love for Matrix.py
@@ -0,0 +1,12 @@
+class Solution:
+    def firstElement(self, n):
+        if n == 0 or n == 1:
+            return 1
+        
+        a, b = 1, 1
+        mod = 1000000007
+        for i in range(n - 2):
+            c = (a + b) % mod
+            a = b % mod
+            b = c % mod
+        return c % mod
